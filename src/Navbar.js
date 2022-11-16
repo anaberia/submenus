a6cb589276c6f5f81f8df0806d16986d460187d5
anaberia <ana.beria3@gmail.com>
2022-11-16 19:18:42 +0400

    add buttons

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,8 +10,22 @@ const Navbar = () => {
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
         </div>
-        <ul className="nav-links"></ul>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn"> products </button>
+          </li>
+          <li>
+            <button className="link-btn"> developers </button>
+          </li>
+          <li>
+            <button className="link-btn"> company </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign in</button>
       </div>
     </nav>
   );
