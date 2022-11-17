2ef332b9710fb6b7dc7c69f2a3f5f3273227acf4
anaberia <ana.beria3@gmail.com>
2022-11-17 13:50:06 +0400

    add condition on submenu component

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { isSubmenuOpen } = useGlobalContext();
+  return (
+    <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}></aside>
+  );
 };
 
 export default Submenu;
